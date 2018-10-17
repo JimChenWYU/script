9493f9a64520b07630d10386e9e566131ce2f642
JimChen <18219111672@163.com>
2018-10-17 23:19:43 +0800

    perf：修改版本号

--- app/Foundation/Application.php
+++ app/Foundation/Application.php
@@ -23,7 +23,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '0.1.0';
+    const VERSION = '1.0.0';
 
     /**
      * @var string
